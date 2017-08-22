5a6140062e22a2c5aaecac32795d81dd85720cea
Mark DeLillo <mdelillo@pivotal.io>
2017-08-22 18:02:46 -0400

    WIP: get nested keys from yaml and deal with errors

--- translate/translate.go
+++ translate/translate.go
@@ -1,9 +1,52 @@
 package translate
 
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"strings"
+	"fmt"
+)
+
+var translations map[interface{}]interface{}
+
 func LoadTranslationFile(path string) error {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %s", path)
+	}
+
+	if err := yaml.Unmarshal([]byte(contents), &translations); err != nil {
+		return fmt.Errorf("failed to parse YAML: %s", contents)
+	}
+
 	return nil
 }
 
-func T(path string) string {
-	return ""
+func T(yamlPath string) (string, error) {
+	splitPath := strings.Split(yamlPath, ".")
+	finalValue := ""
+	m := translations
+	for i, key := range splitPath {
+		value, ok := m[key]
+		if !ok {
+			return "", fmt.Errorf("could not find key: %s", strings.Join(splitPath[0:i+1], "."))
+		}
+
+		stringValue, ok := value.(string)
+		if ok {
+			if i != len(splitPath) - 1 {
+				return "", fmt.Errorf("found string instead of map for key: %s", strings.Join(splitPath[0:i+1], "."))
+			}
+			finalValue = stringValue
+			break
+		}
+
+		mapValue, ok := m[key].(map[interface{}]interface{})
+		if !ok {
+			return "", fmt.Errorf("could not convert value to map for key: %s", strings.Join(splitPath[0:i+1], "."))
+		}
+
+		m = mapValue
+	}
+	return finalValue, nil
 }
--- translate/translate_test.go
+++ translate/translate_test.go
@@ -10,22 +10,24 @@ import (
 )
 
 var _ = Describe("Translate", func() {
-	Describe("T", func() {
+	Describe("Translation", func() {
 		var (
+			language1Translations    string
+			language2Translations    string
 			language1TranslationPath string
 			language2TranslationPath string
 		)
 
-		BeforeEach(func() {
-			language1Translation, err := ioutil.TempFile("", "claimer-translate")
+		JustBeforeEach(func() {
+			file1, err := ioutil.TempFile("", "claimer-translate")
 			Expect(err).NotTo(HaveOccurred())
-			language1TranslationPath = language1Translation.Name()
-			Expect(ioutil.WriteFile(language1TranslationPath, []byte("some: {field: field-in-language1}"), 0644)).To(Succeed())
+			language1TranslationPath = file1.Name()
+			Expect(ioutil.WriteFile(language1TranslationPath, []byte(language1Translations), 0644)).To(Succeed())
 
-			language2Translation, err := ioutil.TempFile("", "claimer-translate")
+			file2, err := ioutil.TempFile("", "claimer-translate")
 			Expect(err).NotTo(HaveOccurred())
-			language2TranslationPath = language2Translation.Name()
-			Expect(ioutil.WriteFile(language2TranslationPath, []byte("some: {field: field-in-language2}"), 0644)).To(Succeed())
+			language2TranslationPath = file2.Name()
+			Expect(ioutil.WriteFile(language2TranslationPath, []byte(language2Translations), 0644)).To(Succeed())
 		})
 
 		AfterEach(func() {
@@ -33,11 +35,93 @@ var _ = Describe("Translate", func() {
 			os.RemoveAll(language2TranslationPath)
 		})
 
-		It("translates based on the loaded file", func() {
-			Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
-			Expect(T("some.field")).To(Equal("field-in-language1"))
-			Expect(LoadTranslationFile(language2TranslationPath)).To(Succeed())
-			Expect(T("some.field")).To(Equal("field-in-language2"))
+		Context("multiple translation files", func() {
+			BeforeEach(func() {
+				language1Translations = "key: field-in-language1"
+				language2Translations = "key: field-in-language2"
+			})
+
+			It("translates based on the loaded file", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				Expect(T("key")).To(Equal("field-in-language1"))
+				Expect(LoadTranslationFile(language2TranslationPath)).To(Succeed())
+				Expect(T("key")).To(Equal("field-in-language2"))
+			})
+		})
+
+		Context("nested keys", func() {
+			BeforeEach(func() {
+				language1Translations = "some: {nested: {key: some-value}}"
+			})
+
+			It("translates using the nested value", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				Expect(T("some.nested.key")).To(Equal("some-value"))
+			})
+		})
+
+		Context("when the translation file does not exist", func() {
+			It("returns an error", func() {
+				Expect(LoadTranslationFile("some-bad-path")).To(MatchError("failed to read file: some-bad-path"))
+			})
+		})
+
+		Context("when the translation file is not valid YAML", func() {
+			BeforeEach(func() {
+				language1Translations = "some-invalid-yaml"
+			})
+
+			It("returns an error", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(MatchError("failed to parse YAML: some-invalid-yaml"))
+			})
+		})
+
+		Context("when the key does not exit", func() {
+			BeforeEach(func() {
+				language1Translations = "---"
+			})
+
+			It("returns an error", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				_, err := T("missingkey")
+				Expect(err).To(MatchError("could not find key: missingkey"))
+			})
+		})
+
+		Context("when the nested key does not exit", func() {
+			BeforeEach(func() {
+				language1Translations = "nested: {key: some-value}"
+			})
+
+			It("returns an error", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				_, err := T("nested.missingkey")
+				Expect(err).To(MatchError("could not find key: nested.missingkey"))
+			})
+		})
+
+		Context("when a value is a string instead of a nested map", func() {
+			BeforeEach(func() {
+				language1Translations = "nested: {key: value}"
+			})
+
+			It("returns an error", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				_, err := T("nested.key.otherkey")
+				Expect(err).To(MatchError("found string instead of map for key: nested.key"))
+			})
+		})
+
+		Context("when a value is not a string or map", func() {
+			BeforeEach(func() {
+				language1Translations = "key: [not-a-string-or-map]"
+			})
+
+			It("returns an error", func() {
+				Expect(LoadTranslationFile(language1TranslationPath)).To(Succeed())
+				_, err := T("key")
+				Expect(err).To(MatchError("could not convert value to map for key: key"))
+			})
 		})
 	})
 })
